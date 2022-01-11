18500e9bc52d00eff14894ba08d2f38c7bbe37c4
Harshawardhan Kamble <kambleharsh08@outlook.com>
2022-01-11 22:17:58 +0530

    polymorphism

---
+++ Day10/Polymorphism/Function overiding/Function overriding.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class base{
+    public:
+virtual void fun1()=0;
+};
+class derived:public base{
+    public:
+    void fun1(){
+        cout<<"Function of derived";
+    }
+};
+int main(){
+    derived b;
+    base *p=&b;
+    p->fun1();
+}
